1dfb15ff6abf29f329f6043d4b9aa3c5f8a1e8a0
reficul31 <reficul31@gmail.com>
2022-11-05 09:33:50 -0400

    Adding react router and pages

--- src/index.js
+++ src/index.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+
+import { SearchPage } from './pages/SearchPage/SearchPage';
+import { UploadPage } from './pages/UploadPage/UploadPage';
+
 import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './index.css';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+        <BrowserRouter>
+            <div className="nav-panel">
+                <div className="nav-inner-div">
+                    <div className="nav-button">
+                        <NavLink to="/">SEARCH</NavLink>
+                    </div>
+                    <div className="nav-button">
+                        <NavLink to="/upload">UPLOAD</NavLink>
+                    </div>
+                </div>
+            </div>
+            <div className="main-panel">
+                <Routes>
+                    <Route path="/" element={<SearchPage />} />
+                    <Route path="/upload" element={<UploadPage />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
